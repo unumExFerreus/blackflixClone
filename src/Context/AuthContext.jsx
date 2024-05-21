709b683453cde1fb3f827fec6241eb5291c21a66
ferreus <pxrxzit@gmail.com>
2024-05-21 15:38:49 +0330

    try to fix the security error

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,51 +1,51 @@
-import { useContext, useEffect, useState } from "react";
-import { createContext } from "react";
-import { auth } from "../firebase";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-} from "firebase/auth";
-import PropTypes from "prop-types";
+// import { useContext, useEffect, useState } from "react";
+// import { createContext } from "react";
+// import { auth } from "../firebase";
+// import {
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+//   signOut,
+//   onAuthStateChanged,
+// } from "firebase/auth";
+// import PropTypes from "prop-types";
 
-const AuthContext = createContext();
+// const AuthContext = createContext();
 
-export function AuthContextProvider({ children }) {
-  const [user, setUser] = useState(null);
+// export function AuthContextProvider({ children }) {
+//   const [user, setUser] = useState(null);
 
-  function signUp(email, password) {
-    return createUserWithEmailAndPassword(auth, email, password);
-  }
+//   function signUp(email, password) {
+//     return createUserWithEmailAndPassword(auth, email, password);
+//   }
 
-  function logIn(email, password) {
-    return signInWithEmailAndPassword(auth, email, password);
-  }
+//   function logIn(email, password) {
+//     return signInWithEmailAndPassword(auth, email, password);
+//   }
 
-  function logOut() {
-    return signOut(auth);
-  }
+//   function logOut() {
+//     return signOut(auth);
+//   }
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+//       setUser(currentUser);
+//     });
+//     return () => {
+//       unsubscribe();
+//     };
+//   }, []);
 
-  return (
-    <AuthContext.Provider value={{ logIn, logOut, signUp, user }}>
-      {children}
-    </AuthContext.Provider>
-  );
-}
+//   return (
+//     <AuthContext.Provider value={{ logIn, logOut, signUp, user }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// }
 
-AuthContextProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+// AuthContextProvider.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
 
-export function UserAuth() {
-  return useContext(AuthContext);
-}
+// export function UserAuth() {
+//   return useContext(AuthContext);
+// }
